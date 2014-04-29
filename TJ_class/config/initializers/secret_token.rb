762318f1529db04b4e300620603b911b7e3da372
Philip <pcharlem01@gmail.com>
2014-04-29 09:35:28 -0400

    we created 2 new resources

---
+++ TJ_class/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TJClass::Application.config.secret_key_base = '40bce93b1cdf90cbdb71b7b187e33563c748ff7f451b9d69b5aa6bf168c278a7ecf0edbcf48f517f7e6c01bfb50d02bf51629c9023db2b3885a23e872e29b8de'
---
+++ TJ_class/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TJClass::Application.config.session_store :cookie_store, key: '_TJ_class_session'
